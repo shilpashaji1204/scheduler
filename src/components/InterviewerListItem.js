1a8a8dbd3188a3110e1308546f952895de5f64bd
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-04-19 15:29:07 -0400

    created new InterviewerListItem.js file and corresponding css file for it to create new component that focused on single interviewer

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classNames from "classnames";
+import 'components/InterviewerListItem.scss';
+
+
+export default function InterviewerListItem(props) {
+
+    let interviewerStyles = classNames("interviewers__items",{
+        "interviewers__item--selected": props.selected,
+    });
+
+    let imageStyles = classNames("interviewer__item-image",{
+        "interviewers__item--selected-image": props.selected,
+    });
+
+    return(
+<li 
+className= {interviewerStyles} onClick={() => props.setInterviewer(props.id)}
+>
+  <img
+    className={imageStyles}
+    src={props.avatar}
+    alt={props.name}
+  />
+  {props.selected && props.name}
+</li>
+  )
+}
