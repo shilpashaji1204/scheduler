58997dd14c7e7f15b6d8732fff8559676232b580
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-04-18 21:18:56 -0400

    Created new DayListItem.test.js file for unit testing and updated DayListItem.js file with a new function called formatSpots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,7 +2,20 @@ import React from "react";
 import 'components/DayListItem.scss';
 import classNames from "classnames";
 
+const formatSpots = spots => {
+
+    if (!spots) {
+      return 'no spots remaining';
+    } else if (spots === 1) {
+      return '1 spot remaining';
+    } else {
+      return `${spots} spots remaining`;
+    }
+  }
+
 export default function DayListItem(props) {
+
+    const spots = formatSpots(props.spots);
 
     const dayClass= classNames('day-list__item', {
         'day-list__item--selected': props.selected,
@@ -12,7 +25,7 @@ export default function DayListItem(props) {
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spots}</h3>
     </li>
   );
 }
