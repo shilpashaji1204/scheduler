7f79ab759f366f3070c9c12f3b0f8c2dc0ab9ac4
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-04-19 21:36:58 -0400

    displays the time for the appointment

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -3,14 +3,12 @@ import React from "react";
 
 export default function Header(props) {
 
-return (
+    return (
 
-    <header className="appointment__time">
-    <h4 className="text--semi-bold">{props.time}</h4>
-    <hr className="appointment__separator" />
-    </header>
+        <header className="appointment__time">
+            <h4 className="text--semi-bold">{props.time}</h4>
+            <hr className="appointment__separator" />
+        </header>
 
-)
-
-  
+    )
 }
