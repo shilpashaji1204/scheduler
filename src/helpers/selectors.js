39c679c40c759a62a590285842b106a33eea89e9
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-04-21 16:50:25 -0400

    Updated to retrieve interviewer data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -14,3 +14,16 @@ export function getAppointmentsForDay(state, day) {
 
         return appointments;
 }
+
+export function getInterview(state, interview) {
+
+    if(!interview) {
+        return null;
+    }
+
+    const interviewer = state.interviewers[interview.interviewer];
+    return {
+        ...interview,
+        interviewer
+    };
+}
