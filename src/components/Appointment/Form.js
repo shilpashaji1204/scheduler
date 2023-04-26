f40b6b3a633f797d19f789ffd2e79d376ac8fc78
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-04-26 15:34:10 -0400

    Updated with validation and test

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,9 +4,10 @@ import Button from "components/Button";
 
 export default function Form(props) {
 
-    const [student, setStudent] = useState(props.student || "");
+    const [student, setStudent] = useState(props.name || "");
     const [interviewer, setInterviewer] = useState(props.interviewer || null);
-
+    const [error, setError] = useState("");
+    
     const reset = () =>  {
         setStudent("");
         setInterviewer(null);
@@ -17,6 +18,21 @@ export default function Form(props) {
         props.onCancel();
     }
 
+    const validate = () => {
+        if (student === "") {
+          setError("Student name cannot be blank");
+          return;
+        }
+
+        if(interviewer === null) {
+            setError("Please select an interviewer");
+            return;
+        }
+
+        setError("");
+        props.onSave(student, interviewer);
+      };
+      
 
     return (
 
@@ -26,12 +42,14 @@ export default function Form(props) {
                 
                     <input
                         className="appointment__create-input text--semi-bold"
-                        name="name"
+                        name={props.name}
                         type="text"
+                        placeholder="Enter Student Name"
                         value={student}
                         onChange={(event) => setStudent(event.target.value)}
-                        placeholder="Enter Student Name"
+                        data-testid="student-name-input"
                     />
+                    <section className="appointment__validation">{error}</section>
                 </form>
                 <InterviewerList
                   interviewers={props.interviewers}
@@ -42,7 +60,7 @@ export default function Form(props) {
             <section className="appointment__card-right">
                 <section className="appointment__actions">
                     <Button danger onClick={cancel}>Cancel</Button>
-                    <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+                    <Button confirm onSubmit={event => event.preventDefault()} onClick={validate}>Save</Button>
                 </section>
             </section>
         </main>
---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appointment from "components/Appointment";
+
+afterEach(cleanup);
+
+describe("Appointment", () => {
+    it("renders without crashing", () => {
+      render(<Appointment />);
+    });
+  });
+
+  it("uses the mock implementation", () => {
+    const fn = jest.fn((a, b) => 42);
+    fn(1, 2);
+    expect(fn).toHaveReturnedWith(42);
+   });
