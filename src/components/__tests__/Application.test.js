2b49ba0b5abe5d55434ada20158b8bbf897a50e2
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-04-27 17:23:32 -0400

    Modified and clean up the code

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -70,34 +70,31 @@ describe("Application", () => {
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
 
-  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
-
+  it('loads data, edits an interview and keeps the spots remaining for Monday the same', async () => {
     // Render the Application.
     const { container } = render(<Application />);
-    //  Wait until the text "Archie Cohen" is displayed.
-    await waitForElement(() => getByText(container, "Archie Cohen"));
-    // Click the "Edit" button on the booked appointment.
-    const appointment = getAllByTestId(container, "appointment").find(
-      appointment => queryByText(appointment, "Archie Cohen")
+      //  Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, 'Archie Cohen'));
+      // Click the "Edit" button on the booked appointment.
+    const appointment = getAllByTestId(container, 'appointment').find(
+      (appointment) => queryByText(appointment, 'Archie Cohen')
     );
 
-    fireEvent.click(getByAltText(appointment, "Edit"));
+    fireEvent.click(getByAltText(appointment, 'Edit'));
+        // Enter the student name and save interview
+    fireEvent.change(getByPlaceholderText(appointment, 'Enter Student Name'), {
+      target: { value: 'Lydia Miller-Jones' }
+    });
 
-    // Click "Sylvia Palmer" as the new interviewer
-    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-    fireEvent.click(getByText(appointment, "Save"));
-    // Wait to see the SAVING
-    expect(getByText(appointment, "saving")).toBeInTheDocument();
+    fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
 
-    await waitForElement(() => getByText(container, "Archie Cohen"))
-    // Check that "Sylvia Palmer" is now displayed as the interviewer
+    fireEvent.click(getByText(appointment, 'Save'));
 
-    expect(getByText(container, "Sylvia Palmer")).toBeInTheDocument();
-    //  Check that the DayListItem with the text "Monday" and keeps the spots remaining
-    const day = getAllByTestId(container, "day").find(day =>
-      queryByText(day, "Monday")
-    );
-    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+    expect(getByText(appointment, (content, element) => content.startsWith('saving'))).toBeInTheDocument();
+      // Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
+    const day = getAllByTestId(container, 'day').find(day => queryByText(day, 'Monday'));
+
+    expect(getByText(day, '1 spot remaining')).toBeInTheDocument();
   });
 
   it("shows the save error when failing to save an appointment", async () => {
